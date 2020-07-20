d099fb2e07e3a65eb8693c78acb526bce5dbeb58
Aurelio Jargas <verde@aurelio.net>
2020-07-21 00:49:03 +0200

    Remove ed alias
    It's not necessary anymore since now in the README there's instructions
    to create a ~/bin/ed symlink.
    
    This solution is more robust, since the alias did not work inside the
    Git config file.

--- .config/fish/conf.d/my.osx.fish
+++ .config/fish/conf.d/my.osx.fish
@@ -4,8 +4,5 @@ add-to-path top /usr/local/opt/coreutils/libexec/gnubin
 # Path to software installed with pip3 install --user
 add-to-path top ~/Library/Python/3.7/bin
 
-# Use brew GNU ed
-alias ed=ged
-
 # txt2regex: use brew gettext binaries
 add-to-path bottom /usr/local/Cellar/gettext/0.19.8.1/bin/
