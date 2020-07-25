4f149c957aa8121eb9c9f05c1d908cb27b5fd10d
Aurelio Jargas <verde@aurelio.net>
2020-07-26 01:26:01 +0200

    Add setup script
    Having the copy/pastable commands in the README was simple and handy.
    
    But since I'm learning the Fish shell, I must create some scripts for
    it. Creating a setup script for the dotfiles was a low hanging
    fruit. It was also fun to do.
    
    Sample run after everything is already setup:
    
    [Clone/update repository]
    Already up to date.
    [Create symlinks]
    ✓ /Users/oreio/.config/fish/conf.d/my.fish is already done
    ✓ /Users/oreio/.config/fish/conf.d/my.git.fish is already done
    ✓ /Users/oreio/.config/fish/functions/add-to-path.fish is already done
    ✓ /Users/oreio/.config/fish/functions/fish_prompt.fish is already done
    ✓ /Users/oreio/.gemrc is already done
    ✓ /Users/oreio/.gitbash is already done
    ✓ /Users/oreio/.gitconfig is already done
    ✓ /Users/oreio/.inputrc is already done
    ✓ /Users/oreio/.screenrc is already done
    ✓ /Users/oreio/.tmux.conf is already done
    ✓ /Users/oreio/.vim is already done
    ✓ /Users/oreio/.vimrc is already done
    ✓ /Users/oreio/.bashrc.osx is already done
    ✓ /Users/oreio/.config/fish/conf.d/my.osx.fish is already done
    [Source dotfiles custom .bashrc from ~/.bashrc]
    ✓ Already done
    [Download .git-prompt.sh for Bash]
    ✓ Already done
    [Create user bin directory]
    ✓ Already done
    [Install diff-so-fancy in user bin]
    ✓ Already done
    [macOS: Add 'ed' symlink so GNU ed can be used as git core.editor]
    ✓ /Users/oreio/bin/ed is already done
    [macOS: Install extra software]
    Please run: brew install coreutils ed fish gnu-sed python shellcheck tig wget
    [Setup Git local configuration (manual intervention needed)]
    ✓ Already done

--- README.md
+++ README.md
@@ -4,64 +4,11 @@ My config files.
 
 I try to be a minimalist. No config fireworks here.
 
-Everything is inside `.bashrc` and there's no `.bash_profile` or `.profile`.
+- Bash: everything is inside `.bashrc` and there's no `.bash_profile` or `.profile`.
+
+- Fish: config files in `.config/fish/conf.d` and `.config/fish/functions`.
 
 
-## Initial setup in a new machine
+## Setup a new machine
 
-```bash
-# All the following commands are home-related
-cd $HOME
-
-# Download
-mkdir -p k/a
-git clone git@github.com:aureliojargas/dotfiles.git k/a/dotfiles
-
-# Do not overwrite default .bashrc
-echo 'test -f ~/k/a/dotfiles/.bashrc && source $_' >> .bashrc
-
-ln -s k/a/dotfiles/.inputrc
-ln -s k/a/dotfiles/.vimrc
-ln -s k/a/dotfiles/.vim
-ln -s k/a/dotfiles/.screenrc
-ln -s k/a/dotfiles/.tmux.conf
-ln -s k/a/dotfiles/.gemrc
-ln -s k/a/dotfiles/.termux
-
-# Git config
-ln -s k/a/dotfiles/.gitconfig
-ln -s k/a/dotfiles/.gitbash
-cp k/a/dotfiles/.gitconfig.local.template .gitconfig.local
-vi .gitconfig.local
-
-# If necessary (Cloud9 already has one)
-test -f .git-prompt.sh || curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > .git-prompt.sh
-
-# Colorful diffs
-test -d bin || mkdir bin
-curl -L https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy > bin/diff-so-fancy
-chmod +x bin/diff-so-fancy
-
-# Fish shell
-mkdir -p .config/fish/{conf.d,functions}
-ln -s ~/{k/a/dotfiles/,}.config/fish/conf.d/my.fish
-ln -s ~/{k/a/dotfiles/,}.config/fish/conf.d/my.git.fish
-ln -s ~/{k/a/dotfiles/,}.config/fish/functions/add-to-path.fish
-ln -s ~/{k/a/dotfiles/,}.config/fish/functions/fish_prompt.fish
-```
-
-Open a new terminal to activate it all.
-
-## Additional setup for macOS
-
-```bash
-ln -s k/a/dotfiles/.bashrc.osx
-
-ln -s ~/{k/a/dotfiles/,}.config/fish/conf.d/my.osx.fish
-
-# Install extra software
-brew install coreutils ed fish gnu-sed python shellcheck tig wget
-
-# GNU ed should be used as the git core.editor's ed
-ln -s /usr/local/bin/ged ~/bin/ed
-```
+    $ fish setup.fish
---
+++ setup.fish
@@ -0,0 +1,173 @@
+# Setup all the dotfiles into a new machine.
+# May be run multiple times in the same machine.
+# Usage: fish setup.fish
+
+set dotfiles_dir $HOME/k/a/dotfiles
+set user_bin $HOME/bin
+
+function is_termux
+    string match -q '*termux*' $HOME
+end
+
+function is_macos
+    test (uname) = Darwin
+end
+
+function log
+    set_color cyan
+    echo "[$argv]"
+    set_color normal
+end
+
+function done
+    if set -q argv[1]
+        set message $argv
+    else
+        set message Done
+    end
+    set_color green
+    echo ✓ $message
+    set_color normal
+end
+
+function already_done
+    if set -q argv[1]
+        set message $argv
+    else
+        set message Already done
+    end
+    set_color yellow
+    echo ✓ $message
+    set_color normal
+end
+
+function failed
+    if set -q argv[1]
+        set message $argv
+    else
+        set message Failed
+    end
+    set_color red
+    echo × $message
+    set_color normal
+end
+
+function insert_line -a line file
+    if grep -F -x -q $line $file
+        already_done
+    else
+        echo $line >>$file
+        done
+    end
+end
+
+function create_symlink -a target symlink
+    if not test (count $argv) -eq 2
+        echo ERROR: usage: create_symlink target symlink
+        exit 1
+    end
+
+    # Make sure the symlink parent dir exists
+    mkdir -p (dirname $symlink)
+
+    if test -L $symlink
+        if test (readlink $symlink) = $target
+            already_done $symlink is already done
+        else
+            failed $symlink is a symlink to an unknown file
+        end
+    else if test -f $symlink
+        failed $symlink is a normal file, skipping symlink creation
+    else if test -d $symlink
+        failed $symlink is a directory, skipping symlink creation
+    else
+        ln -s -v $target $symlink
+    end
+end
+
+#-----------------------------------------------------------------------
+
+set paths \
+    .config/fish/conf.d/my.fish \
+    .config/fish/conf.d/my.git.fish \
+    .config/fish/functions/add-to-path.fish \
+    .config/fish/functions/fish_prompt.fish \
+    .gemrc \
+    .gitbash \
+    .gitconfig \
+    .inputrc \
+    .screenrc \
+    .tmux.conf \
+    .vim \
+    .vimrc
+
+if is_termux
+    set paths $paths \
+        .bashrc.termux \
+        .termux
+end
+
+if is_macos
+    set paths $paths \
+        .bashrc.osx \
+        .config/fish/conf.d/my.osx.fish
+end
+
+log 'Clone/update repository'
+if not test -d $dotfiles_dir
+    mkdir -p (dirname $dotfiles_dir)
+    git clone git@github.com:aureliojargas/dotfiles.git $dotfiles_dir
+else
+    git -C $dotfiles_dir pull
+end
+
+log 'Create symlinks'
+for path_ in $paths
+    create_symlink $dotfiles_dir/$path_ $HOME/$path_
+end
+
+log 'Source dotfiles custom .bashrc from ~/.bashrc'
+insert_line "test -f $dotfiles_dir/.bashrc && source \$_" $HOME/.bashrc
+
+log 'Download .git-prompt.sh for Bash'
+set git_prompt $HOME/.git-prompt.sh
+if test -f $git_prompt
+    already_done
+else
+    curl --location https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh >$git_prompt
+    done
+end
+
+log 'Create user bin directory'
+if test -d $user_bin
+    already_done
+else
+    mkdir -p -v $user_bin
+end
+
+log 'Install diff-so-fancy in user bin'
+set fancy_diff $user_bin/diff-so-fancy
+if test -f $fancy_diff; and test -x $fancy_diff
+    already_done
+else
+    curl -L https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy >$fancy_diff
+    chmod +x $fancy_diff
+    done Installed $fancy_diff
+end
+
+if is_macos
+    log "macOS: Add 'ed' symlink so GNU ed can be used as git core.editor"
+    create_symlink /usr/local/bin/ged $HOME/bin/ed
+
+    log 'macOS: Install extra software'
+    echo Please run: brew install coreutils ed fish gnu-sed python shellcheck tig wget
+end
+
+log 'Setup Git local configuration (manual intervention needed)'
+set git_config_local $HOME/.gitconfig.local
+if test -f $git_config_local
+    already_done
+else
+    cp -v $dotfiles_dir/.gitconfig.local.template $git_config_local
+    echo Please edit $git_config_local
+end
