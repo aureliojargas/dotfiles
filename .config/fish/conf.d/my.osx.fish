8b8a6c61c46487285f80b8bc517ec891cb671dcb
Aurelio Jargas <verde@aurelio.net>
2020-08-25 08:28:43 +0200

    Update brew Python version to 3.8

--- .config/fish/conf.d/my.osx.fish
+++ .config/fish/conf.d/my.osx.fish
@@ -2,7 +2,7 @@
 add-to-path top /usr/local/opt/coreutils/libexec/gnubin
 
 # Path to software installed with pip3 install --user
-add-to-path top ~/Library/Python/3.7/bin
+add-to-path top ~/Library/Python/3.8/bin
 
 # txt2regex: use brew gettext binaries
 add-to-path bottom /usr/local/Cellar/gettext/0.19.8.1/bin/
