4e6cba922dd1b120c0ac0959200643105454b2ae
Aurelio Jargas <verde@aurelio.net>
2021-03-01 00:10:29 +0100

    osx: Add brew ruby to the path (commented)
    I had this in my local clone, but I don't remember exactly why.
    Pushing to save it, just in case.

--- .config/fish/conf.d/my.osx.fish
+++ .config/fish/conf.d/my.osx.fish
@@ -1,6 +1,9 @@
 # Use brew GNU utils by default, without the g prefix: gsed, gwc, ...
 add-to-path top /usr/local/opt/coreutils/libexec/gnubin
 
+# Use brew ruby by default (Ruby 2.7 for Jekyll 4.1.1)
+# add-to-path top /usr/local/opt/ruby/bin
+
 # Path to software installed with pip3 install --user
 add-to-path top ~/Library/Python/3.8/bin
 
