7e58547395f98717a783ccb5c802ebc8210b3ab0
Aurelio Jargas <verde@aurelio.net>
2022-10-07 23:10:03 +0200

    Bump Python version: 3.8 → 3.10

--- .config/fish/conf.d/my.osx.fish
+++ .config/fish/conf.d/my.osx.fish
@@ -5,7 +5,7 @@ add-to-path top /usr/local/opt/coreutils/libexec/gnubin
 # add-to-path top /usr/local/opt/ruby/bin
 
 # Path to software installed with pip3 install --user
-add-to-path top ~/Library/Python/3.8/bin
+add-to-path top ~/Library/Python/3.10/bin
 
 # txt2regex: use brew gettext binaries
 add-to-path bottom /usr/local/Cellar/gettext/0.19.8.1/bin/
