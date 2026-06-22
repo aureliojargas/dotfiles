cd69ee4cf07f9bf657f88668cfa68cfbf0f9d161
Aurelio Jargas <verde@aurelio.net>
2026-06-22 11:39:50 +0200

    osx: Install brew casks in ~/Applications

--- .config/fish/conf.d/my.osx.fish
+++ .config/fish/conf.d/my.osx.fish
@@ -3,3 +3,6 @@ fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin
 
 # Use brew ruby by default (Ruby 2.7 for Jekyll 4.1.1)
 # fish_add_path /usr/local/opt/ruby/bin
+
+# brew casks install in home folder instead of /Applications
+export HOMEBREW_CASK_OPTS="--appdir=$HOME/Applications"
